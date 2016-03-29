4496d1fd3348776c5b51a25a684e00ec3f8c538b
lakatostamas <lakatos.tamas@virgo.hu>
2016-03-29 23:39:37 +0200

    remote

--- config/env/development.js
+++ config/env/development.js
@@ -1,4 +1,4 @@
 module.exports = {
   db: 'mongodb://127.0.0.1:27017/korker',
-  remoteDb: 'mongodb://<admin>:<asdf>@ds035975.mlab.com:35975/korker'
+  remoteDb: 'mongodb://admin:asdf@ds035975.mlab.com:35975/korker'
 };
